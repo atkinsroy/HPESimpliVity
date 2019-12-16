106675f2427593925bf1ffab1ec0bc9581e9f882
Roy Atkins <roy.atkins@hpe.com>
2019-12-17 09:32:21 +1100

    New Utility scripts added

--- UtilityScripts/DailyWeeklyMonthlyReportOriginal.ps1
+++ UtilityScripts/DailyWeeklyMonthlyReportOriginal.ps1
@@ -3,68 +3,101 @@
 # environment.
 
 # Change IP address to match one of your virtual controllers
-$IP = 192.168.1.1
+#$IP = 192.168.1.1
 
-# It is assumed you have previously created a credential file, for example:
+# It is assumed you have previously created a credential file using, for example:
 #Get-Credential -Message 'Enter a password at prompt' -UserName 'administrator@vsphere.local' | Export-Clixml OVCcred.xml
-$Cred = Import-Clixml .\OVCcred.xml
+#$Cred = Import-Clixml .\OVCcred.xml
 
 # Connect is an OmniStack Virtual Controller in your environment:
-Connect-SVT -OVC $IP -Credential $Cred
+#Connect-SVT -OVC $IP -Credential $Cred
 
 # For a report suffix
 $TimeStamp = Get-Date -Format 'yyMMddhhmm'
 
-# Get the dates we want to report on
+# Initialize variables
+$DailyBackup = @()
+$WeeklyBackup = @()
+$MonthlyBackup = @()
+$OldBackup = @()
+$culture = (Get-Culture).DateTimeFormat
+$LocalDate = "$($culture.ShortDatePattern)" -creplace '^d/', 'dd/' -creplace '^M/', 'MM/' -creplace '/d/', '/dd/'
+$LocalTime = "$($culture.LongTimePattern)" -creplace '^h:mm', 'hh:mm' -creplace '^H:mm', 'HH:mm'
+$LocalFormat = "$LocalDate $LocalTime"
+
+# Get the dates we want to report on - days in advance of today, because we're interested in the future expiry of backups
 $Daily = (Get-Date).AddDays(1)
-$Weekly = (Get-Date).AddDays(7)
+$Weekly = (Get-Date).AddDays(7) 
 $Monthly = (Get-Date).AddDays(28)
 
-# Get 'all' backups. This will be limited to 3000 backup objects. To get more objects, 
-# you'll need to filter on some other property (see below), and maybe use multiple commands. 
-$AllBackup = Get-SVTbackup -All -Verbose
+# Get 'all' backups. This example will be limited to 3000 backup objects. So, if you have more than 3000 backup 
+# objects in the federation, you'll need to filter on a property (by backupName, datastore, cluster or VM, see below) 
+# and then iterate, ensuring that each call returns less than 3000 backup objects)
+$AllBackup = Get-SVTbackup -All
 
-Write-Output 'Daily Backups'
-$AllBackup | Where-Object ExpiryDate -le $Daily |
-Export-Csv -Path DailyBackup-$TimeStamp.csv -NoTypeInformation
 
-Write-Output 'Weekly Backups'
-$AllBackup | Where-Object {
-    $_.ExpiryDate -le $Weekly -and $_.ExpiryDate -gt $Daily
-} | Export-Csv -Path WeeklyBackup-$TimeStamp.csv -NoTypeInformation
-
-Write-Output 'Monthly Backups'
-$AllBackup | Where-Object {
-    $_.ExpiryDate -le $Monthly -and $_.ExpiryDate -gt $Weekly
-} | Export-Csv -Path MonthlyBackup-$TimeStamp.csv -NoTypeInformation
-
-Write-Output 'Older Backups'
-$AllBackup | Where-Object ExpiryDate -gt $Monthly |
-Export-Csv -Path OlderBackup-$TimeStamp.csv -NoTypeInformation
-
-Write-Output 'Failed Backups...'
+# For comparison, dates in PowerShell must be "region-neutral", meaning US format.
+# But Get-SVTbackup stores dates as strings honouring the local culture. So to compare with Get-Date, the expiry 
+# date must be parsed to convert it to a "region-neutral" date.
+$AllBackup | ForEach-Object {
+    $ExpiryDate = [datetime]::parse($_.ExpiryDate)
+    if ($ExpiryDate -le $Daily) {
+        #"$ExpiryDate is less than $Daily (Daily)"
+        $DailyBackup += $_
+    }
+    Elseif ($ExpiryDate -gt $Daily -and $ExpiryDate -le $Weekly) {
+        #"$ExpiryDate is greater than $Daily and less than $Weekly (Weekly)"
+        $WeeklyBackup += $_
+    }
+    Elseif ($ExpiryDate -gt $Weekly -and $ExpiryDate -le $Monthly) {
+        #"$ExpiryDate is greater than $Weekly and less than $Monthly (Monthly)"
+        $MonthlyBackup += $_
+    }
+    Else {
+        #"$ExpiryDate is greater $Monthly (Old)"
+        $OldBackup += $_
+    }
+}
+# Report on Failed backups
 $Failed = $AllBackup | Where-Object BackupState -ne 'PROTECTED'
-$FailedCount = ($Failed | Measure-Object).Count
-if ($FailedCount -gt 0) {
-    Write-Warning "$FailedCount failed backups found"
-    $Failed | Export-Csv -Path FailedBackup-$TimeStamp.csv -NoTypeInformation
-}
-else {
-    Write-Output "No failed backups found"
+
+
+
+# Write each collection out to a CSV
+$DailyBackup | Sort-Object { $_.ExpiryDate -as [datetime] } -Descending | Export-Csv -NoTypeInformation -Path BackupDailyReport-$TimeStamp.CSV
+$WeeklyBackup | Sort-Object { $_.ExpiryDate -as [datetime] } -Descending | Export-Csv -NoTypeInformation -Path BackupWeeklyReport-$TimeStamp.CSV
+$MonthlyBackup | Sort-Object { $_.ExpiryDate -as [datetime] } -Descending | Export-Csv -NoTypeInformation -Path BackupMonthlyReport-$TimeStamp.CSV
+$OldBackup | Sort-Object { $_.ExpiryDate -as [datetime] } -Descending | Export-Csv -NoTypeInformation -Path BackupLongTermReport-$TimeStamp.CSV
+If ($Failed) {
+    $Failed | Sort-Object { $_.ExpiryDate -as [datetime] } -Descending | Export-Csv -NoTypeInformation -Path BackupFailedReport-$TimeStamp.CSV
 }
 
-# Filtering on a property of the backup object. Some Examples:
-# by policy name, for example, how to show more than 3000 backups
-# (assumes backups for each policy is less than 3000 backups):
-#  AllBackup = Get-SVTBackup -PolicyName "Bronze" -Limit 3000
-#  AllBackup += Get-SVTBackup -PolicyName "Silver" -Limit 3000
-#  AllBackup += Get-SVTBackup -PoliciyName "Gold" -Limit 3000
-# 
-# by VM, for example:
-#  $Allbackup = Get-SVTBackup -VMname "VM01" -Limit 3000
-#
-# by data store, for example:
+# Write a summary report to the console, with report names for reference.
+"-" * 60 + "`n  Summary Report:"
+"Daily backups, (Expiry date less than 1 day): $($DailyBackup.Count)"
+"Weekly backups, (Expiry date between 1 and 7 days): $($WeeklyBackup.Count)"
+"Monthly backups, (Expiry date between 7 and 28 days): $($MonthlyBackup.Count)"
+"Long Term backups, (Expiry date older than 28 days): $($OldBackup.Count)"
+"Failed backups: $($Failed.Count)"
+"-" * 60
+Get-ChildItem Backup*Report-$TimeStamp.CSV
+
+# Some examples when you have more than 3000 backups in the federation. In each case, you should end up with
+# a full list of backups providing each filtered call to Get-SVTbackup returns less than 3000 backup objects.
+ 
+# Without "hardcoding" the filter property, for example, by each datastore:
+#  Get-SVTdatastore | Foreach-Object {
+#   [array]$AllBackup += Get-SVTBackup -DatastoreName $_ -Limit 3000
+#  }
+
+# by hardcoded data store, for example:
 #  $AllBackup = Get-SVTBackup -DataStoreName "DataStore01" -Limit 3000
-#
-# by cluster, for example:
+#  $AllBackup += Get-SVTBackup -DataStoreName "DataStore02" -Limit 3000
+
+# by hardcoded VM, for example:
+#  $AllBackup = Get-SVTBackup -VMname "VM01" -Limit 3000
+#  $AllBackup += Get-SVTBackup -VMname "VM02" -Limit 3000
+
+# by hardcoded cluster, for example:
 #  $AllBackup = Get-SVTBackup -ClusterName "Production01" -Limit 3000
+#  $AllBackup += Get-SVTBackup -ClusterName "DR01" -Limit 3000
---
+++ UtilityScripts/Get-Support-FileV2.ps1
@@ -0,0 +1,79 @@
+###########################################################################
+# Get-Support-FileV2.ps1
+# 
+# Utility script that automates creation and download of the support file
+# from one or more SimpliVity virtual controllers.
+#
+# This version depends on a third party module called Posh-SSH, but with
+# this you can login using credentials, rather than having to use ssh keys
+#
+# Roy Atkins, HPE Pointnext Services
+###########################################################################
+#
+# You need to install Posh-SSH
+#
+## Install-Module Posh-SSH
+#
+# It is assumed you have previously created a credential file using, for example:
+## Get-Credential -Message 'Enter a password at prompt' -UserName 'administrator@vsphere.local' | Export-Clixml cred.xml
+
+$cred = Import-Clixml .\cred.xml
+
+# Some variables
+$sshcapture = 'source /var/tmp/build/bin/appsetup; /var/tmp/build/cli/svt-support-capture'
+$sshfile = 'ls -pl /core/capture'
+
+# Get a list of OVCs to connect to. You can use import-csv or other methods.
+# $OVC = @('192.168.1.1','192.168.2.1')
+# $OVC = (Get-SVThost).ManagementIP
+$OVC = Import-Csv -Path .\ovclist.csv | Select-Object -ExpandProperty OVC
+
+foreach ($controller in $OVC) {
+    try {
+        New-SSHSession -ComputerName $controller -port 22 -Credential $cred
+    }
+    catch {
+        Write-Warning "Could not establish an SSH session to $controller"
+    }
+}
+
+# Run the capture on all the OVCs together - wait 7 minutes for the capture to complete. This is just
+# to try and avoid the normal command timeout that generatates an error. The captures continue and complete regardless.
+try {
+    "Running capture command on each target virtual controller. This will take up to 7 minutes..."
+    $Session = Get-SSHsession
+    $null = Invoke-SSHcommand -SessionId $Session.SessionId -Command $sshcapture -TimeOut 420
+}
+catch {
+    Write-Warning "Capture command timed out on one or more virtual controllers. We'll need to wait longer it seems."
+}
+
+# Now we think we've got a capture file on each virtual controller. If the capture is not finished, there will still be 
+# a folder, so wait until this is replaced with a capture file.
+foreach ($ThisSession in $Session) {
+    $ThisId = $ThisSession.SessionId
+    $ThisHost = $ThisSession.Host
+    $FolderFound = $true
+    do {
+        $Output = Invoke-SSHcommand -SessionId $ThisId -Command $sshfile | Select-Object -ExpandProperty Output
+        $Output
+
+        $CaptureFile = ($Output | Select-Object -last 1).Split(' ')[-1]
+        "Capture file is $CaptureFile"
+        # Check if the last object is a folder, if so wait.
+        if (($CaptureFile[-1]) -eq '/') {
+            "Wait 30 seconds for capture to complete on $ThisHost..."
+            Start-Sleep 30
+        }
+        else {
+            $FolderFound = $false
+            $CaptureWeb = "http://$ThisHost/capture/$CaptureFile"
+            "Downloading the capture file: $CaptureWeb ..."
+            Invoke-WebRequest -Uri $CaptureWeb -OutFile ".\$CaptureFile"
+        }
+    }
+    While ($FolderFound)
+}
+
+# Cleanup
+$null = Remove-SSHSession -SessionId $Session.SessionId
