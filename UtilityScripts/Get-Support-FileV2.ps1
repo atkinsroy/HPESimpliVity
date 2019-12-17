f31c28e530bd1c012f1c868ee3da8fffc48b59ad
Roy Atkins <roy.atkins@hpe.com>
2019-12-18 09:05:18 +1100

    New utility scripts added

--- UtilityScripts/Get-Support-FileV2.ps1
+++ UtilityScripts/Get-Support-FileV2.ps1
@@ -5,29 +5,39 @@
 # from one or more SimpliVity virtual controllers.
 #
 # This version depends on a third party module called Posh-SSH, but with
-# this you can login using credentials, rather than having to use ssh keys
+# this you can login using credentials, rather than having to upload ssh 
+# keys to each OVC.
 #
 # Roy Atkins, HPE Pointnext Services
 ###########################################################################
 #
 # You need to install Posh-SSH
+# PS:\> Install-Module Posh-SSH
 #
-## Install-Module Posh-SSH 
-#
-# It is assumed you have previously created a credential file using, for example:
-## Get-Credential -Message 'Enter a password at prompt' -UserName 'administrator@vsphere.local' | Export-Clixml cred.xml
+# Get a list of OVCs to connect to. By default, an existing CSV file with 
+# a list of IP addresses is assumed.
+# Other ways:
+# $OVC = @('192.168.1.1','192.168.2.1')
+# $OVC = (Get-SVThost).ManagementIP
+# or specify the -OVC parameter with a comma seperated list of IPs.
+param (
+    [object]$OVC = (Import-CSV -Path .\ovclist.csv | Select-Object -ExpandProperty OVC),
+    [switch]$Silent
+)
 
-$cred = Import-Clixml .\cred.xml
+if ($Silent) {
+    # It is assumed you have previously created a credential file using, for example:
+    # PS:\> Get-Credential -Message 'Enter password' -UserName 'administrator@vsphere.local' | Export-Clixml cred.xml
+    $cred = Import-Clixml .\cred.xml
+}
+else {
+    $cred = Get-Credential -Message 'Enter Password' -UserName 'administrator@vsphere.local'
+}
 
-# Some variables
 $sshcapture = 'source /var/tmp/build/bin/appsetup; /var/tmp/build/cli/svt-support-capture'
 $sshfile = 'ls -pl /core/capture'
 
-# Get a list of OVCs to connect to. You can use import-csv or other methods.
-# $OVC = @('192.168.1.1','192.168.2.1')
-# $OVC = (Get-SVThost).ManagementIP
-$OVC = Import-Csv -Path .\ovclist.csv | Select-Object -ExpandProperty OVC
-
+# Connect to each OVC
 foreach ($controller in $OVC) {
     try {
         New-SSHSession -ComputerName $controller -port 22 -Credential $cred
@@ -37,15 +47,17 @@ foreach ($controller in $OVC) {
     }
 }
 
-# Run the capture on all the OVCs together - wait 7 minutes for the capture to complete. This is just
-# to try and avoid the normal command timeout that generatates an error. The captures continue and complete regardless.
+# Run the capture on all the OVCs together - wait 7 minutes for the capture(s) to complete.
+# The capture(s) continue and complete regardless, but the ssh command may timeout. The Timeout
+# parameter isn't really needed, as we check for completion later - but it does reduce the "wait..."
+# messages generated later.
 try {
     "Running capture command on each target virtual controller. This will take up to 7 minutes..."
     $Session = Get-SSHsession
     $null = Invoke-SSHcommand -SessionId $Session.SessionId -Command $sshcapture -TimeOut 420
 }
 catch {
-    Write-Warning "Capture command timed out on one or more virtual controllers. We'll need to wait longer it seems."
+    Write-Warning "Capture command timed out on one or more virtual controllers. We'll need to wait longer, it seems."
 }
 
 # Now we think we've got a capture file on each virtual controller. If the capture is not finished, there will still be 
@@ -55,21 +67,27 @@ foreach ($ThisSession in $Session) {
     $ThisHost = $ThisSession.Host
     $FolderFound = $true
     do {
-        $Output = Invoke-SSHcommand -SessionId $ThisId -Command $sshfile | Select-Object -ExpandProperty Output
-        $Output
+        try {
+            $Output = Invoke-SSHcommand -SessionId $ThisId -Command $sshfile | Select-Object -ExpandProperty Output
+            $Output
 
-        $CaptureFile = ($Output | Select-Object -last 1).Split(' ')[-1]
-        "Capture file is $CaptureFile"
-        # Check if the last object is a folder, if so wait.
-        if (($CaptureFile[-1]) -eq '/') {
-            "Wait 30 seconds for capture to complete on $ThisHost..."
-            Start-Sleep 30
+            $CaptureFile = ($Output | Select-Object -last 1).Split(' ')[-1]
+            "Capture file is $CaptureFile"
+            # Check if the last object is a folder, if so wait.
+            if (($CaptureFile[-1]) -eq '/') {
+                "Wait 30 seconds for capture to complete on $ThisHost..."
+                Start-Sleep 30
+            }
+            else {
+                $FolderFound = $false
+                $CaptureWeb = "http://$ThisHost/capture/$CaptureFile"
+                "Downloading the capture file: $CaptureWeb ..."
+                Invoke-WebRequest -Uri $CaptureWeb -OutFile ".\$CaptureFile"
+            }
         }
-        else {
+        catch {
             $FolderFound = $false
-            $CaptureWeb = "http://$ThisHost/capture/$CaptureFile"
-            "Downloading the capture file: $CaptureWeb ..."
-            Invoke-WebRequest -Uri $CaptureWeb -OutFile ".\$CaptureFile"
+            Write-Warning "Could not download the support file from $Thishost : $($_.Exception.Message)"
         }
     }
     While ($FolderFound)
